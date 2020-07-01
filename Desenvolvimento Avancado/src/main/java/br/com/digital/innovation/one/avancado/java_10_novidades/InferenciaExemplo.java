9e525ccd843a97c4a4dd8a3e53512dcdc55bf1c9
Bill <billmetal9@gmail.com>
2020-07-01 13:59:36 -0300

    Exemplo para aula de Novidades Java 10 - Inferencia .

---
+++ Desenvolvimento Avancado/src/main/java/br/com/digital/innovation/one/avancado/java_10_novidades/InferenciaExemplo.java
@@ -0,0 +1,41 @@
+package br.com.digital.innovation.one.avancado.java_10_novidades;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class InferenciaExemplo {
+
+	public static void main(String[] args){
+		//printarSoma(5,5,5);
+		connectAndPrintURLJavaOracle();
+	}
+	
+	private static void connectAndPrintURLJavaOracle(){
+		try {
+		var url = new URL("https://docs.oracle.com/javase/10/language/");
+		var urlConnection = url.openConnection();
+		
+			try(var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));){
+				System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void printarSoma(int... numeros) {
+		var soma = 0;
+		if(numeros.length > 0) {
+			for(var numero : numeros) {
+				soma += numero;
+			}
+			System.out.println("A soma é ::"+soma);
+		}
+	}
+
+}
